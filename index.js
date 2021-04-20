3970aa6e9788394cc6b40edd9e98161105df39af
H Nazmul Hassan <71649134+HNazmul@users.noreply.github.com>
2021-04-20 16:18:05 +0600

    nothing added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const MongoClient = require("mongodb").MongoClient;
 const cors = require("cors")
 const port = process.env.PORT || 5000
+require("dotenv").config()
 
 
 
@@ -9,6 +11,41 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.adky9.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect((err) => {
+    const ServiceCollection = client.db(process.env.DB_DATABASE).collection("services");
+
+    app.get("/services", (req, res) => {
+        ServiceCollection.find({}).toArray((error, document) => {
+            if (error) {
+                res.send(error);
+            } else if (document) {
+                res.send(document);
+            }
+        });
+    });
+
+    app.post("/addservice", (req, res) => {
+        ServiceCollection.insertOne(req.body).then((result) => {
+            res.send(result.insertedCount > 0);
+        });
+    });
+
+
+    app.post ("/reviews", (req, res)=> {
+
+
+
+        
+    })
+
+    
+
+
+});
 
 
 
